2e784314060fa9f1904a7b16f8d19016937a502b
Hemantkumar <8114hd@gmail.com>
2026-09-25 00:01:03 +0530

    add

--- src/lib/cookie/cookie.config.js
+++ src/lib/cookie/cookie.config.js
@@ -2,13 +2,13 @@ import config from "../../config/index.js";
 const cookieConfig = {
     accessToken: {
         httpOnly: config.cookie.secure,
-        secure: config.app.isProduction,
+        secure: config.cookie.secure,
         sameSite: config.cookie.sameSite,
         maxAge: config.cookie.accessMaxAge
     },
     refreshToken: {
-        httpOnly:  config.cookie.secure,
-        secure: config.app.isProduction,
+        httpOnly: config.cookie.secure,
+        secure: config.cookie.secure,
         sameSite: config.cookie.sameSite,
         path:config.cookie.path,
         maxAge:config.cookie.refreshMaxAge
