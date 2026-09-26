d6bb88d74338afb07537069f1e49ed76bb363cf6
Hemantkumar <8114hd@gmail.com>
2026-09-27 00:59:53 +0530

    add refresh db

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,88 +9,98 @@ import { sessionRepository } from "../repositories/session.repository.js";
 import { userRepository } from "../repositories/user.repository.js";
 
 export const refresh = async (req, res) => {
-   try {
-      const refreshToken = req.cookies.refreshToken;
+    try {
+        const refreshToken = req.cookies.refreshToken;
 
-      if (!refreshToken) {
-         return res.status(401).json({
+        if (!refreshToken) {
+            return res.status(401).json({
+                success: false,
+                message: "Refresh token required",
+            });
+        }
+
+        // Verify token FIRST
+        const decoded = jwtService.verifyRefreshToken(refreshToken);
+
+        // Then validate token type
+        if (decoded.type !== "refresh") {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid refresh token",
+            });
+        }
+
+        // Find user
+        const user = await userRepository.findById(decoded.sub);
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid refresh token",
+            });
+        }
+
+        // Find session
+        const session = await sessionRepository.findById(decoded.sid);
+
+        if (
+            !session ||
+            session.revokedAt ||
+            session.expiresAt < new Date()
+        ) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthenticated",
+            });
+        }
+
+        // Generate new access token
+        const newAccessToken =
+            jwtService.generateAccessToken({
+                userId: decoded.sub,
+                role: user.role,
+                sessionId: decoded.sid,
+            });
+
+        // Generate new refresh token
+        const newRefreshToken =
+            jwtService.generateRefreshToken({
+                userId: decoded.sub,
+                sessionId: decoded.sid,
+            });
+        const refreshTokenHash = hashService.hashSha256(newRefreshToken);
+        const decode = jwtService.decodeToken(newRefreshToken);
+
+        const updateRefresh = await sessionRepository.updateRefreshToken({
+            sessionId: decode.sid,
+            jti: decode.jti,
+            userId: decode.sub,
+            refreshTokenHash,
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+        })
+        console.log(updateRefresh)
+        // Set rotated refresh token
+        cookieServie.setRefresehToken(
+            res,
+            newRefreshToken
+        );
+
+        return res.status(200).json({
+            success: true,
+            data: {
+                accessToken: newAccessToken,
+            },
+        });
+
+    } catch (error) {
+
+        console.error("REFRESH ERROR:", error);
+
+        return res.status(401).json({
             success: false,
-            message: "Refresh token required",
-         });
-      }
-
-      // Verify token FIRST
-      const decoded = jwtService.verifyRefreshToken(refreshToken);
-
-      // Then validate token type
-      if (decoded.type !== "refresh") {
-         return res.status(401).json({
-            success: false,
-            message: "Invalid refresh token",
-         });
-      }
-
-      // Find user
-      const user = await userRepository.findById(decoded.sub);
-
-      if (!user) {
-         return res.status(401).json({
-            success: false,
-            message: "Invalid refresh token",
-         });
-      }
-
-      // Find session
-      const session = await sessionRepository.findById(decoded.sid);
-
-      if (
-         !session ||
-         session.revokedAt ||
-         session.expiresAt < new Date()
-      ) {
-         return res.status(401).json({
-            success: false,
-            message: "Unauthenticated",
-         });
-      }
-
-      // Generate new access token
-      const newAccessToken =
-         jwtService.generateAccessToken({
-            userId: decoded.sub,
-            role: user.role,
-            sessionId: decoded.sid,
-         });
-
-      // Generate new refresh token
-      const newRefreshToken =
-         jwtService.generateRefreshToken({
-            userId: decoded.sub,
-            sessionId: decoded.sid,
-         });
-
-      // Set rotated refresh token
-      cookieServie.setRefresehToken(
-         res,
-         newRefreshToken
-      );
-
-      return res.status(200).json({
-         success: true,
-         data: {
-            accessToken: newAccessToken,
-         },
-      });
-
-   } catch (error) {
-
-      console.error("REFRESH ERROR:", error);
-
-      return res.status(401).json({
-         success: false,
-         message: "Invalid or expired refresh token",
-      });
-   }
+            message: "Invalid or expired refresh token",
+        });
+    }
 };
 
 
@@ -99,7 +109,7 @@ export const login = async (req, res) => {
         const { email, password } = req.body;
 
         if (!email || !password) {
-           return res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: 'email password required'
             })
@@ -125,11 +135,11 @@ export const login = async (req, res) => {
         const sessionId = new mongoose.Types.ObjectId()
 
         const refreshToken = jwtService.generateRefreshToken({ userId: user._id.toString(), sessionId })
-        const decode=jwtService.decodeToken(refreshToken);
+        const decode = jwtService.decodeToken(refreshToken);
         const refreshTokenHash = hashService.hashSha256(refreshToken);
-        const session = await Session.create({  
-            _id:sessionId,
-            jti:decode.jti,
+        const session = await Session.create({
+            _id: sessionId,
+            jti: decode.jti,
             userId: user._id,
             refreshTokenHash,
             expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
@@ -155,16 +165,16 @@ export const login = async (req, res) => {
     } catch (error) {
         console.error("login error:", error)
 
-       return res.status(500).json({
+        return res.status(500).json({
             success: false,
             message: 'internal server error'
         })
     }
 }
 
-export const register=async (req, res) => {
+export const register = async (req, res) => {
     try {
-        const { name, email, password,username } = req.body;
+        const { name, email, password, username } = req.body;
         if (!email || !password || !name || !username) {
             return res.status(401).json({
                 success: false,
@@ -194,7 +204,7 @@ export const register=async (req, res) => {
             data: {
                 user: {
                     id: newUser._id,
-                    username:newUser.username,
+                    username: newUser.username,
                     name: newUser.name,
                     email: newUser.email,
                     role: newUser.role
@@ -204,17 +214,17 @@ export const register=async (req, res) => {
     } catch (error) {
         console.error("signup error:", error)
 
-       return res.status(500).json({
+        return res.status(500).json({
             success: false,
             message: 'internal server error'
         })
     }
 }
-export const logout=async (req, res) => {
+export const logout = async (req, res) => {
     try {
-        const {_id}=req.session;
-        
-        const session=await sessionRepository.revoke(_id)
+        const { _id } = req.session;
+
+        const session = await sessionRepository.revoke(_id)
         return res.status(200).json({
             success: true,
             data: null
@@ -222,7 +232,7 @@ export const logout=async (req, res) => {
     } catch (error) {
         console.error("Logout Error", error)
 
-       return res.status(500).json({
+        return res.status(500).json({
             success: false,
             message: 'internal server error'
         })
